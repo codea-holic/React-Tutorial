af81795b75bbcdb7ab7efc51785cd6f2dd4ff1a2
Ganesh Singh <krganesh1611@gmail.com>
2022-04-06 13:32:44 +0530

    Update AddTask and TaskList

--- src/index.js
+++ src/index.js
@@ -3,35 +3,76 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class AddTask extends React.Component {
+	// constructor(props) {
+	// 	super(props);
+	// }
 	render() {
 		return (
 			<>
-			<form>
-				<input type="text"></input>
-				<input type="button" value="Add Task"></input>
-			</form>
+				<form>
+					<input type="text"></input>
+					<input type="button" value="Add Task"></input>
+				</form>
 			</>
 		);
 	}
 }
 
 class TaskList extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 	render() {
+		let list = [];
+		
+		for (let i = 0; i < this.props.tasks.length; i++) {
+			let task = this.props.tasks[i];
+			let li = (<li key={i}>{task.desc}</li>);
+			list.push(li);
+		}
+		console.log(list);
 		return (
-			<div className={this.props.forStyling}>{this.props.purpose}</div>
+			<div className={this.props.forStyling}>
+				<div>{this.props.purpose}</div>
+				<ul>
+					{list}
+				</ul>
+			</div>
 		);
 	}
 }
 
 class App extends React.Component {
-
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: [
+				{
+					desc: 'Switch off light',
+					isfinished: true
+				}, {
+					desc: 'Turn on fan',
+					isfinished: false
+				}, {
+					desc: 'Make tea',
+					isfinished: true
+				}, {
+					desc: 'Take dinner',
+					isfinished: false
+				}
+			]
+		}
+	}
 	render() {
+		let tasks = this.state.tasks;
+		let todo = tasks.filter(t => t.isfinished === false);
+		let finished = tasks.filter(t => t.isfinished === true);
 		return (
 			<>
 				<div className='add-task'><AddTask /></div>
 				<div className='task-lists'>
-					<TaskList purpose="Task to do" forStyling="todo"/>
-					<TaskList purpose="Finished Tasks" forStyling="finished"/>
+					<TaskList tasks={todo} purpose="Task to do" forStyling="todo" />
+					<TaskList tasks={finished} purpose="Finished Tasks" forStyling="finished" />
 				</div>
 			</>
 		);
